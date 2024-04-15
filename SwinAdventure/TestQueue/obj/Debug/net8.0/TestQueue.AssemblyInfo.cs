efd76bd06467bb804148e06e0b3d7798d86cf075
Duy Tan Pham <tanphamduy24082005@gmail.com>
2024-04-15 13:04:55 +1000

    change name

--- SwinAdventure/TestQueue/obj/Debug/net8.0/TestQueue.AssemblyInfo.cs
+++ SwinAdventure/TestQueue/obj/Debug/net8.0/TestQueue.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestQueue")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5744b35381558dcc86a5c8c56f2d5beb5251b4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efcdb00025712f4ef19611bfd3ae03fa69f6f850")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestQueue")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestQueue")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
