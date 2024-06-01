59278aa7268cf4225e64691cd147350a573eb6c4
Duy Tan Pham <tanphamduy24082005@gmail.com>
2024-06-02 01:44:39 +1000

    ATM Initial Commit

---
+++ ATM Machine/ATM/ATM/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set the console title and text color
+            Console.Title = "My ATM Application";
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // Display the welcome message
+            Console.WriteLine("\n\n-----------------------Welcome to my ATM App-----------------------\n\n");
+
+            //Prompt the user to enter account number and password
+
+
+        }
+    }
+}
